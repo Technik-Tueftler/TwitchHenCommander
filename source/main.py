179ed58cb90988313fccbed9b83908a6188f6c2e
Technik Tüftler <technik.tueftler@gmail.com>
2023-09-17 21:58:51 +0200

    separate hashtags

--- source/main.py
+++ source/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import asyncio
 import os
 from twitchio.ext import commands
 
@@ -8,44 +9,72 @@ token = os.getenv("TOKEN")
 nickname = os.getenv("NICKNAME")
 initial_channels = [os.getenv("INIT_CHANNELS")]
 
+hashtags = set()
+lock = asyncio.Lock()
+
+
+async def check_if_broadcaster(message) -> bool:
+    if message.author.is_broadcaster:
+        return True
+    return False
+
+
+async def check_if_authorized(message) -> bool:
+    if message.author.is_broadcaster:
+        return True
+    if message.author.is_mod:
+        return True
+    if message.author.is_vip:
+        return True
+    return False
+
+
+async def separate_hash(message) -> set:
+    new_hashtags = set(
+        element.replace("#", "")
+        for element in message.content.lower().split(" ")
+        if element.startswith("#") and len(element) >= 3
+    )
+    return new_hashtags
+
 
 class Bot(commands.Bot):
-
     def __init__(self):
-        # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
-        # prefix can be a callable, which returns a list of strings or a string...
-        # initial_channels can also be a callable which returns a list of strings...
-        super().__init__(token=token, prefix='?', initial_channels=initial_channels)
+        super().__init__(token=token, prefix="!", initial_channels=initial_channels)
 
     async def event_ready(self):
-        # Notify us when everything is ready!
-        # We are logged in and ready to chat and use commands...
-        print(f'Logged in as | {self.nick}')
-        print(f'User id is | {self.user_id}')
+        print(f"Logged in as | {self.nick}")
+        print(f"User id is | {self.user_id}")
 
     async def event_message(self, message):
-        # Messages with echo set to True are messages sent by the bot...
-        # For now we just want to ignore them...
+        # Messages with echo set to True are messages sent by the bot
         if message.echo:
             return
-
-        # Print the contents of our message to console...
         print(message.content)
+        if await check_if_authorized(message):
+            global hashtags  # pylint: disable=global-statement
+            async with lock:
+                hashtags |= await separate_hash(message)
 
-        # Since we have commands and are overriding the default `event_message`
-        # We must let the bot know we want to handle and invoke our commands...
         await self.handle_commands(message)
 
     @commands.command()
     async def hello(self, ctx: commands.Context):
-        # Here we have a command hello, we can invoke our command with our prefix and command name
-        # e.g ?hello
-        # We can also give our commands aliases (different names) to invoke with.
+        await ctx.send(f"Hello {ctx.author.name}!")
 
-        # Send a hello back!
-        # Sending a reply back to the channel is easy... Below is an example.
-        await ctx.send(f'Hello {ctx.author.name}!')
+    @commands.command()
+    async def finish(self, ctx: commands.Context):
+        await ctx.send("Bot wird beendet.")
+        await self.close()
+        print("Bot wird beendet")
+        exit(0)
 
 
-bot = Bot()
-bot.run()
+def main() -> None:
+    bot = Bot()
+    bot.run()
+    print("Bot wird beendet")
+
+
+if __name__ == "__main__":
+    main()
