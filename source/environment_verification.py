49986163a0da68ba2843442a5d2c5397fe42198e
Technik Tüftler <technik.tueftler@gmail.com>
2023-09-21 23:00:39 +0200

    - verification of the environment variables - register hashtags

--- source/environment_verification.py
+++ source/environment_verification.py
@@ -1,9 +1,36 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os
+
+client_id = os.getenv("CLIENT_ID", None)
+token = os.getenv("TOKEN", None)
+nickname = os.getenv("NICKNAME", None)
+init_channels = os.getenv("INIT_CHANNELS", None)
+
+app_settings = {"ID": client_id, "token": token, "nickname": nickname, "channels": None}
+
+
+def check_env_available() -> bool:
+    if None not in (client_id, token, nickname, init_channels):
+        return True
+    return False
+
+
+def check_config_available() -> bool:
+    return False
+
+
+def setting_verification() -> bool:
+    if check_env_available():
+        app_settings["channels"] = init_channels.split(",")
+        return True
+    if check_config_available():
+        return True
+    return False
 
 
 def main() -> None:
-    pass
+    setting_verification()
 
 
 if __name__ == "__main__":
--- source/main.py
+++ source/main.py
@@ -1,79 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import asyncio
-import os
-from twitchio.ext import commands
+import environment_verification as env
+from source.twitch_bot import Bot
 
-client_id = os.getenv("CLIENT_ID")
-token = os.getenv("TOKEN")
-nickname = os.getenv("NICKNAME")
-initial_channels = [os.getenv("INIT_CHANNELS")]
-
-hashtags = set()
-lock = asyncio.Lock()
-test_list = []
+data = {"tweets": []}
 
 
-async def check_if_broadcaster(message) -> bool:
-    if message.author.is_broadcaster:
-        return True
-    return False
+async def my_second_task():
+    while True:
+        print("sec Task")
+        await asyncio.sleep(3)
 
 
-async def check_if_authorized(message) -> bool:
-    if message.author.is_broadcaster:
-        return True
-    if message.author.is_mod:
-        return True
-    if message.author.is_vip:
-        return True
-    return False
+def main():
+    if not env.setting_verification(): return
+    bot = Bot(env.app_settings, data)
+    loop = asyncio.get_event_loop()
+    bot_task = loop.create_task(bot.start())
 
+    # second_task = loop.create_task(my_second_task())
 
-async def separate_hash(message) -> set:
-    new_hashtags = set(
-        element.replace("#", "")
-        for element in message.content.lower().split(" ")
-        if element.startswith("#") and len(element) >= 3
-    )
-    return new_hashtags
-
-
-class Bot(commands.Bot):
-    def __init__(self):
-        super().__init__(token=token, prefix="!", initial_channels=initial_channels)
-
-    async def event_ready(self):
-        print(f"Logged in as | {self.nick}")
-        print(f"User id is | {self.user_id}")
-
-    async def event_message(self, message):
-        # Messages with echo set to True are messages sent by the bot
-        if message.echo:
-            return
-        print(message.content)
-        if await check_if_authorized(message):
-            global hashtags  # pylint: disable=global-statement
-            async with lock:
-                hashtags |= await separate_hash(message)
-
-        await self.handle_commands(message)
-
-    @commands.command()
-    async def hello(self, ctx: commands.Context):
-        await ctx.send(f"Hello {ctx.author.name}!")
-
-    @commands.command()
-    async def finish(self, ctx: commands.Context):
-        await ctx.send("Bot wird beendet.")
-        await self.close()
-        exit(0)
-
-
-def main() -> None:
-    bot = Bot()
-    bot.run()
-    print("Bot wird beendet")
+    try:
+        loop.run_until_complete(asyncio.gather(bot_task))
+        # loop.run_until_complete(asyncio.gather(bot_task, second_task))
+    except KeyboardInterrupt:
+        bot_task.cancel()
+        # second_task.cancel()
+        loop.run_until_complete(asyncio.gather(bot_task, second_task, return_exceptions=True))
+    finally:
+        loop.close()
 
 
 if __name__ == "__main__":
