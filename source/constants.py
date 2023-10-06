d0fbb3f9faa936a635655eb0016ce3b4dd832c42
Technik Tüftler <technik.tueftler@gmail.com>
2023-10-06 15:42:49 +0200

    - correct the key for hashtag length. Now it is possible to configure the hashtag length via config file - cleanup

--- source/constants.py
+++ source/constants.py
@@ -8,4 +8,4 @@ HASHTAG_MAX_LENGTH = 10
 HASHTAG_MIN_LENGTH = 3
 TWEET_MAX_LENGTH = 280
 TWEET_START_STRING = "Highlights aus dem Stream: "
-TWEET_END_STRING = ""
+TWEET_END_STRING = "Danke, dass ihr heute da wart."
