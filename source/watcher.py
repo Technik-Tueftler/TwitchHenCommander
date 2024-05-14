8834dff746e54afd19dd7abca178044251b1db0e
Technik Tüftler <technik.tueftler@gmail.com>
2024-05-14 16:29:32 +0200

    create init function

--- source/watcher.py
+++ source/watcher.py
@@ -1,5 +1,8 @@
+"""All functions and features for logging the app
+"""
 from loguru import logger
 
-logger.add("../files/henCommander.log", rotation="500 MB", level="INFO")
-
-temp = 4/0
+async def init_logging() -> None:
+    """Initialization of logging to create log file and set level at beginning of the app.
+    """
+    logger.add("../files/henCommander.log", rotation="500 MB", level="INFO")
