491fdeeff068f827c5f3f5809222a8f14e12a1d4
Technik Tüftler <technik.tueftler@gmail.com>
2024-12-31 15:19:51 +0100

    add new log level for separate ratelimit

--- source/watcher.py
+++ source/watcher.py
@@ -1,7 +1,9 @@
 """All functions and features for logging the app
 """
 import sys
+from functools import partialmethod
 from loguru import logger
+
 
 def init_logging(log_level: str) -> None:
     """Initialization of logging to create log file and set level at beginning of the app.
@@ -10,5 +12,7 @@ def init_logging(log_level: str) -> None:
         log_level (str): Configured log level
     """
     logger.remove()
+    logger.level("EXTDEBUG", no=9, color="<bold><blue>")
+    logger.__class__.extdebug = partialmethod(logger.__class__.log, "EXTDEBUG")
     logger.add("../files/henCommander.log", rotation="500 MB", level=log_level)
     logger.add(sys.stdout, colorize=True, level=log_level)
